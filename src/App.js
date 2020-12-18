afebbcfb71891465e7ff9d8954661c4327b91418
unknown <kavyaraju20@gmail.com>
2020-12-18 11:19:40 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+import Home from './Home.js';
+import Header from './Header.js';
+import Footer from './Footer.js';
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import SearchPage from './SearchPage.js';
+
+function App() {
+  return (
+    <div className="app">
+      <Router>
+         <Header />
+         <Switch>
+          
+          <Route path="/search">
+           <SearchPage />
+          </Route>
+          
+           <Route path="/">
+              <Home />
+           </Route>
+
+         </Switch>
+         <Footer />
+      </Router>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <div className="footer">
+            <p>&copy; 2020 Airbnb clone</p>
+            <p>Privacy . Terms . Sitemap . Company Details</p>
+            
+        </div>
+    )
+}
+
+export default Footer;
